9d1309f65ce7ff6fe9a808593c98da535bda628d
Teaom <dclarkjenkins87@gmail.com>
2023-05-21 22:47:24 -0600

    finished seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,7 +2,8 @@ const sequelize = require('../config/connection');
 const { User, Project } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,10 +13,20 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
+  let posts = [1, 2, 3]
+
+  for (const post of postData) {
+    await post.create({
+      ...post,
+      userId: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  for (const comment of commentData) {
+    await comment.create({
+      ...comment,
+      userId: users[Math.floor(Math.random() * users.length)].id,
+      postId: posts[Math.floor(Math.random() * users.length)],
     });
   }
 
