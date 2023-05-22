6fe4126cf6b5bb40a24224f4fcea28991a1204fd
Teaom <dclarkjenkins87@gmail.com>
2023-05-22 00:10:56 -0600

    init commit handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -34,16 +34,17 @@ router.get('/post/:id', async (req, res) => {
         User,
         {
           model: Comment,
+          include: [User],
         },
       ],
     });
 
     const post = postData.get({ plain: true });
-res.json(post)
-    // res.render('singlePost', {
-    //   post,
-    //   logged_in: req.session.logged_in
-    // });
+// res.json(post)
+    res.render('singlePost', {
+      post,
+      logged_in: req.session.logged_in
+    });
   } catch (err) {
     res.status(500).json(err);
   }
